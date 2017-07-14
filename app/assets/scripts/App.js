d479a2826f9cc3dc7189ba3b7d61357e96550f88
Manuel <manuelalaminosd@gmail.com>
2017-07-14 22:28:32 +0100

    Added webpack. Set webpack automation with gulp

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,8 @@
+var Person = require('./modules/Person');
+
+alert("Testing 1 2 3");
+
+var john = new Person("John Doe", "blue");
+john.greet();
+var jane = new Person("Jane Smith", "green");
+jane.greet();
